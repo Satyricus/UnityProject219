2d3227b7bd25bd403f9bf2f7e0c08d4861ccb4a3
Jon Erik McWilliams <jon_mcw@outlook.com>
2015-09-02 13:28:02 +0200

    Added a placeholder player. Movement and animations are in place and functional with 'wasd' keys.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rbody;	// Player rigidbody
+	Animator anim;		// Player amimator
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));	// getAxisRaw = bool
+		if (movement_vector != Vector2.zero) {	// Player is moving
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat("inputX", movement_vector.x);
+			anim.SetFloat("inputY", movement_vector.y);
+		} else {
+			anim.SetBool("isWalking", false);
+		}
+
+		rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime);	// Move player's rigidbody
+	}
+}
