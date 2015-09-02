78a6d23e854a8580c573b6428538ecde42d6d91e
hussam samir yousif <searock91@gmail.com>
2015-09-02 22:22:53 +0200

    Now we have a main menu, a scene order and you can build and run the project.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+*
+* This script manages Main menu and the buttons on main menu, configurations screen should have .
+* Main menu should be scene (level) 0.
+* Configurations Should be scene 1.
+* First playable scene should be scene 2
+* ...
+*/
+public class MainMenu : MonoBehaviour {
+
+    /*public void LoadConfig()
+    {
+        Application.LoadLevel(1);
+    }*/
+
+    public void loadLevelTwo()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void LoadLevelThree()
+    {
+        Application.LoadLevel(2);
+    }
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
