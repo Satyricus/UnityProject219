2d3c27962eff4e1e576a08819118f4c867e3844c
Jon Erik McWilliams <jon_mcw@outlook.com>
2015-11-16 14:14:00 +0100

    Tidied up the project (removed empty folders and removed unused  json plugins and scripts). Also fixed a bug that cause Fireball to spawn player into a cavern/dungeon. Also renamed dungeon scripts from dungeone to dungeon

--- Assets/Scripts/Dungeon/EnterDungeone.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EnterDungeone : MonoBehaviour {
-
-	void OnTriggerEnter2D(Collider2D other) {
-		Application.LoadLevel(3);
-	}
-}
--- Assets/Scripts/Dungeon/EnterDungeoneHardMode.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EnterDungeoneHardMode : MonoBehaviour {
-
-	void OnTriggerEnter2D(Collider2D other) {
-		Application.LoadLevel(4);
-	}
-}
