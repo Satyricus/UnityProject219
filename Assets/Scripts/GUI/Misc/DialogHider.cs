252aff84f9db064d70f32c35decc8cbcb35a4958
hussam samir yousif <searock91@gmail.com>
2015-12-08 16:03:46 +0100

    Fixed GUI bugs.

---
+++ Assets/Scripts/GUI/Misc/DialogHider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogHider : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject Dialog;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Application.loadedLevel != 2)
+			Dialog.SetActive(false);
+
+		else 
+			Dialog.SetActive(true);
+	
+	}
+}
