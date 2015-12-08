17790ea2ca4c14bf73ee515a0f58057c1844c1a9
hussam samir yousif <searock91@gmail.com>
2015-12-08 11:29:09 +0100

    Reworked how NPC's act, now they will look in the direction you talk to them at. Also created a couple of NPC's, will create more later.

---
+++ Assets/Scripts/Player/Levels.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levels : MonoBehaviour {
+
+	int currentLevel;
+
+	// Use this for initialization
+	void Start () {
+		currentLevel = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/npc/NPC.cs
+++ Assets/Scripts/npc/NPC.cs
@@ -12,9 +12,12 @@
 	private string[] currentDialog; 
 
 
+
+	private Transform npcTransform;
 	private GameObject GameGui;
 	private GameObject Dialog;
     private Text UItext;
+	private Image panel;
 
     private int current;
 	private int lastLine;
@@ -23,22 +26,36 @@
 
 	private GameObject Player;
 
-	GameObject MovableChars;
-	GamePause pause;
+
+	SpriteRenderer spriteRenderer;
+
+	[SerializeField]
+	private Sprite up;
+
+	[SerializeField]
+	private Sprite down;
+
+	[SerializeField]
+	private Sprite left;
+
+	[SerializeField]
+	private Sprite right;
 
     // Use this for initialization
     void Start ()
-    {
-		MovableChars = GameObject.Find ("MovableCharacters");
-		pause = MovableChars.GetComponent<GamePause> ();
-
-
+	{
 		GameGui = GameObject.Find ("GameGui");
 		Dialog = GameObject.Find ("Dialog");
 		UItext = Dialog.GetComponentInChildren<Text> ();
+		panel = Dialog.GetComponentInChildren<Image>();
 		Player = GameObject.Find ("Player");
+		npcTransform = GetComponent<Transform>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 
-		Dialog.SetActive (false);
+
+		spriteRenderer.sprite = down;
+
+		removeDialog ();
 		currentDialog = dialogOne;
 
         hasTalked = false;
@@ -49,7 +66,7 @@
     // Update is called once per frame
     void Update () {
 		if (current == lastLine + 1) {
-			Dialog.SetActive(false);
+			removeDialog();
 			HasTalked();
 		}
 
@@ -60,16 +77,22 @@
 		currentDialog = dialogTwo;
 		lastLine = currentDialog.Length;
 		current = 0;
-		pause.UnlockCharacters();
 	}
 
+	private void removeDialog() {
+		UItext.enabled = false;
+		panel.enabled = false;
+	}
+
+	private void showDialog() {
+		UItext.enabled = true;
+		panel.enabled = true;
+	}
 
 	void Talk() {
-		pause.LockCharacters();	
-
 		if(current < currentDialog.Length)
 		{
-			Dialog.SetActive(true);
+			showDialog();
 			UItext.text = currentDialog[current];
 		}
 		current++;
@@ -77,9 +100,40 @@
 
     void OnTriggerStay2D(Collider2D coll) {
 		if (Input.GetKeyDown((inputKey))) {
+			ChangeLookingDirection();
 			Talk ();
 	}
 
-		//Dialog.SetActive (false);        
     }
+
+	private void ChangeLookingDirection() {
+		var playerPosition = Player.transform.position;
+		var npcPosition = npcTransform.transform.position;
+		float xDifference = Mathf.Abs( npcPosition.x - playerPosition.x);
+		float yDifference = Mathf.Abs( npcPosition.y - playerPosition.y);
+
+
+
+		if (npcPosition.x > playerPosition.x && xDifference > yDifference) {
+			spriteRenderer.sprite = left;
+		}
+		
+		if (npcPosition.x < playerPosition.x && xDifference > yDifference) {
+			spriteRenderer.sprite = right;
+		}
+
+		if (npcPosition.y > playerPosition.y && yDifference > xDifference) {
+			spriteRenderer.sprite = down;
+		}
+
+		if (npcPosition.y < playerPosition.y && yDifference > xDifference) {
+			spriteRenderer.sprite = up;
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		current = 0;
+		removeDialog();
+	}
 }
