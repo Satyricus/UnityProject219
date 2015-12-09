20d644d27c0f151f52072a54b8c0d32db60358da
hussam samir yousif <searock91@gmail.com>
2015-12-09 19:36:00 +0100

    I remapped Haste and Potions, Haste is now 5 and potions are now 6. Furthermore I added a gui component for potions in the lower left corner that shows how many potions we have.

---
+++ Assets/Scripts/Player/PotionsGui.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PotionsGui : MonoBehaviour {
+
+
+	private Text potionText;
+
+	GameObject player;
+	PlayerStats pstats;
+
+	// Use this for initialization
+	void Start () {
+		potionText = GetComponentInChildren<Text>();
+
+		player = GameObject.Find ("Player");
+		pstats = player.GetComponent<PlayerStats>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		potionText.text = "Potions: " + pstats.getHealthPotions();
+	}
+}
