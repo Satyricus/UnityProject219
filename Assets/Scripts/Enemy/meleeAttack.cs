ddbe51d0e35e0f9e36403cc8509fc46e83206579
Alexander Mjøs <amj003@student.uib.no>
2015-12-10 16:40:45 +0100

    Created Golem boss easy, medium and hard.

---
+++ Assets/Scripts/Enemy/meleeAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meleeAttack : MonoBehaviour {
+	private int attackDamage;
+	Animator anim;
+	GameObject player;
+	EnemyStats estats;
+	private float attackStart;
+	private float attackCD;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		anim = GetComponent<Animator> ();
+		estats = GetComponent<EnemyStats> ();
+		attackCD = 1.0f;
+		attackStart = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.GetBool ("isAttacking") && Time.time > attackStart + attackCD) {
+			attackDamage = estats.GetAttackDamage();
+			attackStart = Time.time;
+			player.GetComponent<PlayerStats>().TakeDamage(attackDamage);
+		}
+	}
+}
