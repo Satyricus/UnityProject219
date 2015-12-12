cffe32afc0555da4ce7747b465120aa7685b843b
Alexander Mjøs <amj003@student.uib.no>
2015-12-12 21:16:14 +0100

    Fixed bug where bat and bosses could walk though walls. Added pause functionality. We can now pause the game using "p" on the keyboard or "start" at xbox 360 controller :)

--- Assets/Scripts/Misc/GamePause.cs
+++ Assets/Scripts/Misc/GamePause.cs
@@ -12,11 +12,28 @@ using System.Collections;
 public class GamePause : MonoBehaviour {
      
     private bool pause;
+	public string pauseKey;
+	public GameObject pauseImage;
+	GameObject player;
+	GameObject pObj;
 
 	// Use this for initialization
 	void Start () {
+		player = GameObject.Find ("Player");
 	    pause = false;
 
+	}
+	void Update(){
+		if (Input.GetKeyDown (pauseKey) || Input.GetKeyDown (KeyCode.Joystick1Button7)){
+			if(Time.timeScale > 0.5){
+				pObj = Instantiate(pauseImage,player.transform.position,Quaternion.identity) as GameObject;
+				Time.timeScale = 0;
+			}
+			else{
+				Destroy(pObj);
+				Time.timeScale = 1;
+			}
+		}
 	}
 
 	public bool GetPausStatus() {
